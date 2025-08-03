5cc0ad0c744da05202e3add84d26493f330f6dd8
luiscastro193 <luiscastro193@gmail.com>
2025-08-03 15:12:14 +0200

    Add files

---
+++ main.js
@@ -0,0 +1,94 @@
+"use strict";
+import * as distributions from 'https://luiscastro193.github.io/PRNG/distributions.js';
+
+const PI2 = Math.PI * 2;
+const randomNormal = distributions.toNormal(Math.random);
+const N = 15;
+const lifeExpectancy = 60 * 1000;
+const lifeDeviation = 10 * 1000;
+const randomLife = distributions.toGamma(lifeExpectancy, lifeDeviation, Math.random);
+const randomSpawn = distributions.toGamma(lifeExpectancy / N, lifeDeviation / N, Math.random);
+const randomSize = distributions.toLogNormal(.03, .005, Math.random);
+const randomAlpha = distributions.toBeta(.85, .1, Math.random);
+const randomPosition = distributions.toBeta(.5, .25, Math.random);
+const steerDeviation = 5e-4 * PI2;
+const randomSteer = () => steerDeviation * randomNormal();
+const randomSpeed = distributions.toLogNormal(.01 / 1000, .005 / 1000, Math.random);
+const randomAcceleration = distributions.toGamma(1, 1e-4, Math.random);
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+let width, height, referenceLength;
+
+function module(n, m) {
+	return ((n % m) + m) % m;
+}
+
+class Firefly {
+	constructor(newborn, lifeDiff) {
+		this.remainingLife = randomLife();
+		
+		if (newborn)
+			this.remainingLife += lifeDiff;
+		else
+			this.remainingLife *= Math.random();
+		
+		if (this.remainingLife > 0) {
+			this.size = randomSize();
+			this.alpha = randomAlpha();
+			this.x = randomPosition();
+			this.y = randomPosition();
+			this.theta = PI2 * Math.random();
+			this.speed = randomSpeed();
+		}
+	}
+	
+	draw(time) {
+		this.remainingLife -= time;
+		
+		if (this.remainingLife > 0) {
+			this.theta += randomSteer();
+			this.speed *= randomAcceleration();
+			this.x = module(this.x + this.speed * time * Math.cos(this.theta), 1);
+			this.y = module(this.y + this.speed * time * Math.sin(this.theta), 1);
+			
+			ctx.beginPath();
+			ctx.arc(this.x * width, this.y * height, this.size * referenceLength, 0, PI2);
+			ctx.fillStyle = `rgba(255, 255, 255, ${this.alpha})`;
+			ctx.fill();
+			
+			return true;
+		}
+	}
+}
+
+function resizeCanvas() {
+	const size = canvas.getBoundingClientRect();
+	canvas.width = width = Math.ceil(size.width * devicePixelRatio);
+	canvas.height = height = Math.ceil(size.height * devicePixelRatio);
+	referenceLength = Math.min(width, height);
+}
+
+addEventListener("resize", resizeCanvas);
+resizeCanvas();
+
+let drawObjects = Array.from({length: N}, () => new Firefly(false));
+let spawnTime = randomSpawn();
+let lastTime = performance.now();
+
+function draw(time) {
+	const timeDiff = time - lastTime;
+	lastTime = time;
+	spawnTime -= timeDiff;
+	
+	while (spawnTime <= 0) {
+		drawObjects.push(new Firefly(true, spawnTime + timeDiff));
+		spawnTime += randomSpawn();
+	}
+	
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawObjects = drawObjects.filter(object => object.draw(timeDiff));
+	requestAnimationFrame(draw);	
+}
+
+requestAnimationFrame(draw);
